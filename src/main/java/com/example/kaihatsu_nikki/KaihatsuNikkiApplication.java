968074beb59463622af3baed812293fb371c502d
Krishna <krishnatejabakaraju@gmail.com>
2025-10-12 17:35:37 +0530

    added spring initializer

---
+++ src/main/java/com/example/kaihatsu_nikki/KaihatsuNikkiApplication.java
@@ -0,0 +1,13 @@
+package com.example.kaihatsu_nikki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaihatsuNikkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaihatsuNikkiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kaihatsu_nikki/KaihatsuNikkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kaihatsu_nikki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaihatsuNikkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
